91ff34e0481e9e21ad2fcb8b056af19408c9657b
Facu <facucarp100093@gmail.com>
2020-09-30 21:33:46 -0300

    Primer commit!

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Renderer fondo;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.04f,0) * Time.deltaTime;
+    }
+}
